527a274bb8251c9cb7ced2e644917eced6e33d88
Gustavo Copaga <84489895+Copagoide@users.noreply.github.com>
2023-03-31 00:06:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$con = mysqli_connect("sql307.epizy.com","epiz_33904857","NN4UOjZpZJ3gtX","epiz_33904857_poftfolio");
+$DB_HOST = $_ENV["$DB_HOST"];
+$DB_USER = $_ENV["$DB_USER"];
+$DB_PASSWORD = $_ENV["$DB_PASSWORD"];
+$DB_NAME = $_ENV["$DB_NAME"];
+$DB_PORT = $_ENV["$DB_PORT"];
+$con = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 //mysqli_select_db($con,"portfolio");
 
 $data = json_decode(file_get_contents("php://input"));
